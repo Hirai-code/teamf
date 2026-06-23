dcf0df9d399af076b6e2da8ed12a3795d0862aec
sak064 <sak064@sakschool.onmicrosoft.com>
2026-06-23 13:19:47 +0900

    アカウントURL直打ち

--- src/main/java/portal/servlet/AccountEditorServlet.java
+++ src/main/java/portal/servlet/AccountEditorServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import portal.dao.AccountDao;
 import portal.dto.AccountDto;
@@ -20,6 +21,15 @@ public class AccountEditorServlet extends HttpServlet {
             HttpServletResponse response)
             throws ServletException, IOException {
 
+    	HttpSession session = request.getSession(false);
+
+    	if (session == null
+    	        || session.getAttribute("loginUser") == null) {
+
+    	    response.sendRedirect(
+    	            request.getContextPath() + "/login");
+    	    return;
+    	}
 
         request.setCharacterEncoding("UTF-8");
 
